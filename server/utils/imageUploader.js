6e8aa58dba11736fa614610e96aa47ffbed58d41
KarishmaGR <Karishmasvryadav@gmail.com>
2023-10-04 22:25:39 +0530

    added studybuddy on cloude

---
+++ server/utils/imageUploader.js
@@ -0,0 +1,29 @@
+const cloudinary = require("cloudinary").v2
+
+exports.uploadImageToCloudinary = async (file, folder, height, quality) => {
+  const options = { folder }
+  if (height) {
+    options.height = height
+  }
+  if (quality) {
+    options.quality = quality
+  }
+  options.resource_type = "auto"
+  console.log("OPTIONS", options)
+  return await cloudinary.uploader.upload(file.tempFilePath, options)
+}
+
+
+exports.deleteImageToCloudinary = async (file, folder, height, quality) => {
+  const options = { folder }
+  if (height) {
+    options.height = height
+  }
+  if (quality) {
+    options.quality = quality
+  }
+  options.resource_type = "auto"
+  console.log("OPTIONS", options)
+  return await cloudinary.uploader.destroy(file.tempFilePath, options)
+
+}
